2c46af21a8e9262b81afe921b47d9673e286c239
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-04-09 00:36:43 +0530

    Prime number program added

---
+++ src/algorithm_day16_17/PrimeNumber.java
@@ -0,0 +1,38 @@
+package algorithm_day16_17;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public void primeNumber(int first , int second) {
+        int i , j , flag;
+
+        for(i=first;i<=second;i++) {
+            if(i==0 || i==1)
+                continue;
+            flag = 1;
+
+            for(j=2;j<=i/2;++j) {
+                if(i%j == 0) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag == 1)
+                System.out.println(i+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Lower Bound: ");
+        int first = scan.nextInt();
+
+        System.out.println("Upper Bound: ");
+        int second = scan.nextInt();
+
+        PrimeNumber prime = new PrimeNumber();
+        prime.primeNumber(first,second);
+    }
+}
